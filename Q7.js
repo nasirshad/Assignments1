1ccc1e2f3a7982726ecdc17e942750e132f425a3
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-02 10:10:21 +0500

    Message

---
+++ Q7.js
@@ -0,0 +1,5 @@
+"use strict";
+var num1 = 800;
+var num2 = 40;
+var div = num1 / num2;
+console.log(div);
---
+++ Q7.ts
@@ -0,0 +1,4 @@
+var num1:number=800;
+var num2:number=40;
+var div:number=num1 / num2;
+console.log(div);
