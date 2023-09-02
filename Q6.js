3e8d9a2091f90335cda097ee93bc1bfa1669ec92
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-02 10:05:53 +0500

    Message

---
+++ Q6.js
@@ -0,0 +1,13 @@
+"use strict";
+var num1 = 100;
+var num2 = 202;
+var num3 = 330;
+var num4 = 404;
+var num5 = 55;
+var num6 = 69;
+var num7 = 87;
+var num8 = 98;
+var num9 = 900;
+var num10 = 110;
+var multiply = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10;
+console.log(multiply);
---
+++ Q6.ts
@@ -0,0 +1,12 @@
+var num1:number=100;
+var num2:number=202;
+var num3:number=330;
+var num4:number=404;
+var num5:number=55;
+var num6:number=69;
+var num7:number=87;
+var num8:number=98;
+var num9:number=900;
+var num10:number=110;
+var multiply:number=num1*num2*num3*num4*num5*num6*num7*num8*num9*num10;
+console.log(multiply)
