e45f165b61c6c020685631956ac6644034b15ba6
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-02 10:15:57 +0500

    Message

---
+++ Q8.js
@@ -0,0 +1,8 @@
+"use strict";
+var num1 = 10;
+var num2 = 220;
+var num3 = 300;
+var num4 = 8;
+var num5 = 8;
+var result = num1 * num2 + num3 / num4 - num5;
+console.log(result);
---
+++ Q8.ts
@@ -0,0 +1,7 @@
+var num1:number=10;
+var num2:number=220;
+var num3:number=300;
+var num4:number=8;
+var num5:number=8;
+var result:number=num1*num2+num3/num4-num5;
+console.log(result);
