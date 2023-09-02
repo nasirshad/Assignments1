f4af75d6931e4cc18a9724d3f106ce723f6873b6
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-02 10:00:42 +0500

    Message

---
+++ Q5.js
@@ -0,0 +1,13 @@
+"use strict";
+var num1 = 10;
+var num2 = 20;
+var num3 = 30;
+var num4 = 40;
+var num5 = 50;
+var num6 = 60;
+var num7 = 70;
+var num8 = 80;
+var num9 = 90;
+var num10 = 100;
+var subtract = num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10;
+console.log(subtract);
---
+++ Q5.ts
@@ -0,0 +1,12 @@
+var num1:number=10
+var num2:number=20
+var num3:number=30
+var num4:number=40
+var num5:number=50
+var num6:number=60
+var num7:number=70
+var num8:number=80
+var num9:number=90
+var num10:number=100
+var subtract:number=num1-num2-num3-num4-num5-num6-num7-num8-num9-num10
+console.log(subtract)
